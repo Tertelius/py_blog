167fef5acf3e577bcf5886556c8c2412b01928bb
Tertelius <61936083+Tertelius@users.noreply.github.com>
2021-05-17 09:52:35 +0300

    Добавили вывод постов общим списком и по одному.

--- app.py
+++ app.py
@@ -50,5 +50,20 @@ def create_article():
         return render_template("create-article.html")
 
 
+@app.route('/posts')
+def posts ():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    # articles = Article.query.first()
+    return render_template("posts.html", articles=articles) #Передаем в шаблон постс данные через переменную артиклс
+    # в принципе ее можно было бы назвать как угодно, однако, принято использовать такое же обозначение, для меньшей
+    # путаницы.
+
+
+@app.route('/posts/<int:id>')
+def posts_detail (id):
+    article = Article.query.get(id)
+    return render_template("posts_detail.html", article=article)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
